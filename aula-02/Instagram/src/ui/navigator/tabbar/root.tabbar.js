93fddd31c15ba5bd0a805e0e7ac4a298579f2abb
Philippe Santos Silveira <philippe.silveira@cwi.com.br>
2019-04-03 12:42:56 -0300

    fix tab-bar navigation

--- aula-02/Instagram/src/ui/navigator/tabbar/root.tabbar.js
+++ aula-02/Instagram/src/ui/navigator/tabbar/root.tabbar.js
@@ -7,8 +7,6 @@ import { FeedStack } from '../../navigator/stacks/feed'
 import { TabBar } from '../../components'
 
 import { FEED_ROUTES } from '../routes/feed/feed.route'
-import { DIRECT_ROUTES } from '../routes/direct/direct.route'
-import { EDIT_PROFILE_ROUTES } from '../routes/edit-profile/edit-profile.route'
 
 export const RootTabBar = createBottomTabNavigator({
     FeedStack: {
@@ -18,9 +16,6 @@ export const RootTabBar = createBottomTabNavigator({
         tabBarComponent: ({ navigation }) => (
             <TabBar navigation={navigation}
                 onPressHome={() => navigation.navigate(FEED_ROUTES.FEED)}
-                onPressSearch={() => navigation.navigate(DIRECT_ROUTES.DIRECT)}
-
-                onPressUser={() => navigation.navigate(EDIT_PROFILE_ROUTES.EDIT_PROFILE)}
             />)
     }
 )
